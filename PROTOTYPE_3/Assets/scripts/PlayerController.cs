bf4e91e76116bbe54d584e2ab719896b8feb0864
jordanchicksen1 <2333640@students.wits.ac.za>
2025-04-17 16:45:53 +0200

    did a fair amount of movement

---
+++ PROTOTYPE_3/Assets/scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+namespace BumpyJump.TheCharacterController
+{
+    [DefaultExecutionOrder(-1)]
+    public class PlayerController : MonoBehaviour
+    {
+
+        [Header("Components")]
+        [SerializeField] private CharacterController _characterController;
+        [SerializeField] private Camera _playerCamera;
+
+        [Header("Base Movement")]
+        public float runAcceleration = 0.25f;
+        public float runSpeed = 4f;
+        public float drag = 0.1f;
+
+        [Header("Camera Settings")]
+        public float lookSenseH = 0.1f;
+        public float lookSenseV = 0.1f;
+        public float lookLimitV = 89f;
+
+        private PlayerLocomotionInput _playerLocomotionInput;
+        private Vector2 _cameraRotation = Vector2.zero;
+        private Vector2 _playerTargetRotation = Vector2.zero;
+        private void Awake()
+        {
+            _playerLocomotionInput = GetComponent<PlayerLocomotionInput>();
+        }
+
+        private void Update()
+        {
+            Vector3 cameraForwardXZ =  new Vector3(_playerCamera.transform.forward.x, 0f, _playerCamera.transform.forward.z).normalized;
+            Vector3 cameraRightXZ = new Vector3(_playerCamera.transform.right.x, 0f, _playerCamera.transform.right.z).normalized;
+            Vector3 movementDirection = cameraRightXZ * _playerLocomotionInput.MovementInput.x + cameraForwardXZ * _playerLocomotionInput.MovementInput.y;
+
+            Vector3 movementDelta = movementDirection * runAcceleration * Time.deltaTime;
+            Vector3 newVelocity = _characterController.velocity + movementDelta;
+
+            //add drag to player
+            Vector3 currentDrag = newVelocity.normalized * drag * Time.deltaTime;
+            newVelocity = (newVelocity.magnitude > drag * Time.deltaTime) ? newVelocity - currentDrag : Vector3.zero;
+            newVelocity = Vector3.ClampMagnitude(newVelocity, runSpeed);
+
+            //move character 
+            _characterController.Move(newVelocity * Time.deltaTime);
+        }
+
+    }
+}
+
